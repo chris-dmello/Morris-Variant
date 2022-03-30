dcd46418548bb001155e8e5383056352bdd7f1ef
Christopher D'Mello <c.d.dmello@gmail.com>
2022-03-29 20:32:21 -0500

    Minimax WIP

--- MinMaxOpening.java
+++ MinMaxOpening.java
@@ -4,6 +4,16 @@ import java.util.ArrayList;
 
 public class MinMaxOpening {
 
+    private class MinMaxTreeNode {
+        public Board board;
+        public int score;
+        public ArrayList<Board> children;
+
+        MinMaxTreeNode(Board board){
+            this.board = board;
+        }
+    }
+
     public static void main(String[] args) {
         if (args.length != 3) {
             System.out.println("Please provide the correct arguments.");
@@ -26,25 +36,39 @@ public class MinMaxOpening {
             return;
         }
 
-        Board output = minMaxOpening(board, depth, true);
+        Board output = miniMaxOpening(board, depth, true);
         System.out.println("Board Position: " + output.toString());
 
     }
 
-    public static Board minMaxOpening(Board board, int depth, boolean isMax) {
+    public static Board miniMaxOpening(Board board, int depth, boolean isMax) {
+        // Find Best Child and Return it
         if (depth == 0)
             return chooseOpening(MoveGenerator.generateMovesOpening(board), isMax);
 
-        //ArrayList<Board> moves = MoveGenerator.generateMovesOpening(board);
-        //MoveGenerator.generateMovesOpening(board);
+        // Find all next moves. These will be the children.
+        ArrayList<Board> children = MoveGenerator.generateMovesOpening(board);
+        ArrayList<Board> grandchildren = new ArrayList<Board>();
         
+        // Fill grandchildren list
+        for (int i = 0; i < children.size(); i++) {
+            grandchildren.set(i, chooseOpening(MoveGenerator.generateMovesOpening(children.get(i)), !isMax));
+        }
 
-        // return chooseOpening(moves, !isMax);
-        return null;
+        // Select temporary "best" child at first 
+        Board bestChild = children.get(0); // TODO: handle null case
+        
+        // Choose the best board using a for loop.
+        for (int i = 0; i < children.size(); i++) {
+            
+        }
 
+        // Find "best" grandchild
+        // Return parent of best gradnchild
+        return bestChild;
     }
 
-    public static Board chooseOpening(ArrayList<Board> L, boolean max) {
+    public static Board chooseOpening(ArrayList<Board> L, boolean isMax) {
         if (L.size() == 0)
             return null;
         Board chosen = L.get(0);
@@ -52,15 +76,19 @@ public class MinMaxOpening {
 
         for (int i = 0; i < L.size(); i++) {
             int i_heuristic_score = StaticEstimator.Opening(L.get(i).countWhite(), L.get(i).countBlack());
-            if (max & i_heuristic_score > chosen_heuristic_score) {
+            if (isMax & i_heuristic_score > chosen_heuristic_score) {
                 chosen = L.get(i);
                 chosen_heuristic_score = i_heuristic_score;
-            } else if (!max & i_heuristic_score < chosen_heuristic_score) {
+            } else if (!isMax & i_heuristic_score < chosen_heuristic_score) {
                 chosen = L.get(i);
                 chosen_heuristic_score = i_heuristic_score;
             }
         }
 
         return chosen;
+    }
+
+    public static int staticEstimate(Board board) {
+        return StaticEstimator.Opening(board.countWhite(), board.countBlack());
     }
 }
