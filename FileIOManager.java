2d3274cc5386b351c8eeaf81614b50385d16ce35
chris-dmello <c.d.dmello@gmail.com>
2022-03-31 19:00:02 -0500

    Final clean up for Minimax and AB

--- FileIOManager.java
+++ FileIOManager.java
@@ -1,5 +1,7 @@
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -14,5 +16,12 @@ class FileIOManager {
         
         Board board = new Board(line);
         return board;
+    }
+    
+    public static void writeOutput(String out, String content) throws IOException {
+        BufferedWriter writer = new BufferedWriter(new FileWriter(out));
+        writer.write(content);
+
+        writer.close();
     }
 }
